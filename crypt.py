b7ce07d4387a7aa45af098a2de8eab28b76ec294
aleksandrs12 <90208363+aleksandrs12@users.noreply.github.com>
2022-04-10 19:20:49 +0300

    Update crypt.py

--- crypt.py
+++ crypt.py
@@ -1,37 +1,6 @@
-import time
-import pandas as pd
-import pandas_datareader.data as web
-import datetime
-import os
-
-
-pd.core.common.is_list_like = pd.api.types.is_list_like
-
 my_key = 484
-start = datetime.datetime(2020, 1, 1)
-end = datetime.datetime(2020, 1, 2)
-SP50 = web.DataReader(['sp500'], 'fred', start, end)
 
 
-
-def random_num(limit):
-    t = str(time.time())
-    pd.core.common.is_list_like = pd.api.types.is_list_like
-
-
-    try:
-        start = datetime.datetime(2012 + int(t[-2]), int(t[-1])+1, 1)
-        end = datetime.datetime(2012 + int(t[-2]), int(t[-1])+1, 2)
-        SP50 = web.DataReader(['sp500'], 'fred', start, end)
-        for n in range(len(str(SP50))):
-            if str(SP50)[n] == '.':
-                a = int(round(int(str(SP50)[n+1:n+3])  / (100 / limit)))
-                a + 10
-                return a
-        #return int(round(int(t[-3:-1])  / (100 / limit)))
-        return random_num(limit)
-    except:
-        return random_num(limit)
 
 
 
@@ -55,30 +24,8 @@ for n in range(len(alp)):
 
 
 
-a = str(SP50)
-
-def random_num(limit):
-    t = str(time.time())
-    pd.core.common.is_list_like = pd.api.types.is_list_like
-
-
-    try:
-        start = datetime.datetime(2012 + int(t[-2]), int(t[-1])+1, 1)
-        end = datetime.datetime(2012 + int(t[-2]), int(t[-1])+1, 2)
-        SP50 = web.DataReader(['sp500'], 'fred', start, end)
-        for n in range(len(str(SP50))):
-            if str(SP50)[n] == '.':
-                a = int(round(int(str(SP50)[n+1:n+3])  / (100 / limit)))
-                a + 10
-                return a
-        #return int(round(int(t[-3:-1])  / (100 / limit)))
-        return random_num(limit)
-    except:
-        return random_num(limit)
-
 
 def crypt(key):
-    l = len(alp)
     f = open("text.txt", "r")
     f1 = f.read()
     f1 = f1.strip()
@@ -96,4 +43,4 @@ o = crypt(77)
 
 f = open("text_crypted.txt", "w")
 f.write(o)
-    
+  
